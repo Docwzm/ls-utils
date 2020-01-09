83c02d1e80a9ca2c3e2b39ca7d9014bdd93cd70f
Yusup64 <sufu.yu@lifesense.com>
2020-01-09 14:43:18 +0800

    新增根据租户动态添加指令

--- src/vue/directives.js
+++ src/vue/directives.js
@@ -1,5 +1,5 @@
 import { getQueryString } from '../common'
-export const TenantDirective = {
+export const TenantHideDirective = {
     inserted(el, binding) {
         let tn = getQueryString(location.href, "tn");
         let whiteList = {}.toString.call(binding.value) === "[object Array]" ? binding.value : [binding.value];
@@ -7,5 +7,17 @@ export const TenantDirective = {
             el.style.display = "none";
             el.parentElement.removeChild(el);
         }
+    }
+}
+// 根据指令动态修改DOM样式
+export const TenantClassDirective = {
+    inserted(el, binding) {
+        let tn = getQueryString(location.href, "tn");
+        let classList;
+        classList = {}.toString.call(binding.value) === "[object Array]" ? binding.value : [binding.value];
+        classList.forEach(item => {
+            el.classList.add(`${item}`);
+            tn && el.classList.add(`${item}-${tn}`);
+        });
     }
 }
